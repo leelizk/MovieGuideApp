078628094167f91b9d6ddc91915d7e7d5b4989c2
lizk <lizk123>
2020-11-20 07:41:39 +0800

    2020-11-20 db test insert

--- app/src/main/java/com/example/movieguideapp/base/StartApp.kt
+++ app/src/main/java/com/example/movieguideapp/base/StartApp.kt
@@ -2,9 +2,23 @@ package com.example.movieguideapp.base
 
 import android.app.Application
 import android.util.Log
+import com.example.movieguideapp.data.local.AlbumDao
+import com.example.movieguideapp.data.local.PhotoDao
+import com.example.movieguideapp.data.model.Album
+import com.example.movieguideapp.data.model.Photo
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class StartApp : Application() {
     private val TAG:String = StartApp::class.java.simpleName;
+
+    @Inject
+    lateinit var photoDao: PhotoDao;
+
+    @Inject
+    lateinit var albumDao:AlbumDao;
+
     override fun onCreate() {
         super.onCreate()
         init()
@@ -15,6 +29,23 @@ class StartApp : Application() {
         Log.d(TAG,"init...")
         //初始化数据库 or something else here
         AppDatabaseProvider(this).provideAppDataBase()
+        var albumList:MutableList<Album> = mutableListOf();
+
+        var album:Album = Album(1,"相册"+1,"我的","试试","2020-11-20",1,"lizk");
+        albumList.add(album);
+
+        GlobalScope.launch {
+            albumDao.insertAll(albumList);
+        }
+
+        var photoList:MutableList<Photo> = mutableListOf();
+        for(i in 1..11){
+            var photo:Photo = Photo(1,1,"测试1","","");
+            photoList.add(photo);
+        }
+        GlobalScope.launch {
+            photoDao.insertAll(photoList);
+        }
     }
 
 
