2c8909264d0febc759804c26b4f711368505c251
ex_lizk4 <leelizk@aliyun.com>
2020-11-25 13:30:00 +0800

    2020-11-25 增加fragment

--- app/src/main/java/com/example/movieguideapp/ui/AlbumListActivity.kt
+++ app/src/main/java/com/example/movieguideapp/ui/AlbumListActivity.kt
@@ -1,18 +1,15 @@
 package com.example.movieguideapp.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.movieguideapp.R
-import com.example.movieguideapp.data.vo.AlbumTwoItem
 import com.example.movieguideapp.databinding.ActivityAlbumlistBinding
-import com.example.movieguideapp.ui.adapter.AlbumListAdapter
 import com.example.movieguideapp.ui.viewmodel.AlbumListViewModel
-import com.example.movieguideapp.ui.viewmodel.BaseActivityViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AlbumListActivity : AppCompatActivity() {
@@ -20,13 +17,20 @@ class AlbumListActivity : AppCompatActivity() {
     companion object{
         val TAG:String = AlbumListActivity::class.java.simpleName;
     }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
-    //lateinit var mBinding:ActivityAlbumlistBinding;
+    lateinit var binding:ActivityAlbumlistBinding;
     private val albumListVm:AlbumListViewModel by viewModel();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //mBinding=DataBindingUtil.setContentView(this, R.layout.fragment_album_list);
+        binding=DataBindingUtil.setContentView(this, R.layout.fragment_album_list);
+        navController = findNavController(R.id.container)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         initHandler(viewModel = albumListVm)
     }
 
