472aa7d33bec42c481c7be6d628afb8446b71e0c
lizk <lizk123>
2021-01-05 05:27:39 +0800

    2021  fix directions not gernate

--- app/src/main/java/com/example/movieguideapp/ui/viewmodel/AlbumListNaviagtionViewModel.kt
+++ app/src/main/java/com/example/movieguideapp/ui/viewmodel/AlbumListNaviagtionViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.movieguideapp.ui.viewmodel
 
+import com.example.movieguideapp.ui.fragment.AlbumDetailFragmentDirections
+import com.example.movieguideapp.ui.fragment.AlbumFragmentDirections
+
 class AlbumListNaviagtionViewModel : NavigationViewModel() {
 
     fun showDetail() {
-        /*navigationCommandStream.value = NavigationCommand.ShowFragment(
-                directions = AlbumList.showOpenSourceLicencesFragment(LibsBuilder())
-        )*/
+        navigationCommandStream.value = NavigationCommand.ShowFragment(
+            directions = AlbumFragmentDirections.albumDetailAction()
+        )
     }
 
-    fun showPlay() {
-
+    fun showPlay(url: String) {
+        navigationCommandStream.value = NavigationCommand.ShowFragment(
+            directions = AlbumDetailFragmentDirections.playerAction(url)
+        )
     }
 
 }
